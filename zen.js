57d57b80003c1193b69ef5350c26afa324df064f
Anthony Kelley <kelley.anthonyk@gmail.com>
2024-05-22 22:25:59 -0500

    update2

---
+++ zen.js
@@ -0,0 +1,23 @@
+const headleft = document.querySelector(".head_left_text");
+headleft.textContent = "CSS ZEN GARDEN"
+headleft.style.color = "white"
+
+const headLBtm = document.querySelector(".head_left_Btm_text");
+headLBtm.textContent = "The Beauty of CSS Design"
+headLBtm.style.color = "white"
+
+
+const headright = document.querySelector(".head_right_text");
+headright.textContent = "VIEW ALL DESIGNS"
+headright.style.color = "red"; // VERTICAL DROPDOWN???
+
+/*HEADER ABOVE------------------------------------------------------------------*/
+
+/*AUDIO----------------------------------------------------------------------*/
+ document.getElementById("audio").volume = 0.2
+
+ //set timeout
+
+ /*ICONS----------------------------------------------*/
+ 
+ 
